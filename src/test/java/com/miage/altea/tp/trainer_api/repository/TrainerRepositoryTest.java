8c5cae4eee714603a04ebb8c88c5cd5cc779140d
Chikhoune Sellenia <chikhoune@b13p14.fil.univ-lille1.fr>
2019-03-11 12:02:06 +0100

    add security

--- src/test/java/com/miage/altea/tp/trainer_api/repository/TrainerRepositoryTest.java
+++ src/test/java/com/miage/altea/tp/trainer_api/repository/TrainerRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.repository.CrudRepository;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
